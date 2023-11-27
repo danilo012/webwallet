0bcc599dd944887442afbbad7f407c92463f0e10
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-11-27 16:43:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -25,4 +25,18 @@ $card.on("click", function(){
   } 
 });
 
+const textElement = document.getElementById("text");
+const copyButton = document.getElementById("copy");
 
+const copyText = (e) => {
+  window.getSelection().selectAllChildren(textElement);
+  document.execCommand("copy");
+  e.target.setAttribute("tooltip", "Copied! ✅");
+};
+
+const resetTooltip = (e) => {
+  e.target.setAttribute("tooltip", "Copy to clipboard");
+};
+
+copyButton.addEventListener("click", (e) => copyText(e));
+copyButton.addEventListener("mouseover", (e) => resetTooltip(e));
