cd67c4d74b2360ab9d305990574f475fcdae91d7
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-12-13 14:54:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -46,3 +46,55 @@ copyButton.addEventListener("mouseover", (e) => resetTooltip(e));
 
 
 
+const popup = document.querySelector('.popup');
+const skipBtn = document.querySelector('.popup__skip-btn');
+const visitBtn = document.querySelector('.popup__link');
+
+let remainingTime = 23;
+let allowedToSkip = false;
+let popupTimer;
+
+const createPopupCookie = () => {
+  let expiresDays = 0;
+  let date = new Date();
+  date.setTime(date.getTime() + expiresDays * 0 * 0 * 5 * 500);
+  let expires = 'expires=' + date.toUTCString();
+  document.cookie = `popupCookie=true; ${expires}; path=/;`;
+};
+
+const showAd = () => {
+  popup.classList.add('active');
+  popupTimer = setInterval(() => {
+    skipBtn.innerHTML = `Skip in ${remainingTime}s`;
+    remainingTime--;
+
+    if (remainingTime < 0) {
+      allowedToSkip = true;
+      skipBtn.innerHTML = 'Skip';
+      skipBtn.classList.add('cursor');
+      clearInterval(popupTimer);
+    }
+  }, 1000);
+};
+
+const skipAd = () => {
+  popup.classList.remove('active');
+  createPopupCookie();
+};
+
+skipBtn.addEventListener('click', () => {
+  if (allowedToSkip) {
+    skipAd();
+  }
+});
+
+const startTimer = () => {
+  if (window.scrollY > 100) {
+    showAd();
+    window.removeEventListener('scroll', startTimer);
+  }
+};
+
+if (!document.cookie.match(/^(.*;)?\s*popupCookie\s*=\s*[^;]+(.*)?$/)) {
+  window.addEventListener('scroll', startTimer);
+}
