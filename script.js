f3d04f7d7bfbaa57e03dbc11a14a331d4f64bb46
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-12-13 16:45:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,3 @@
-/*
-  Inspired from - https://dribbble.com/shots/6713486-2-FREE-Touch-Gesture-Animations-interactions-for-after-effects
-*/
-const timeline = new TimelineLite({ paused: true, reversed: true }),
-      $title = $(".cards .title"),
-      $card = $(".card-wrapper .card"),
-      $transactions = $(".transactions"),
-      transitionSpeed = .5;
-
-timeline.to(".card-wrapper", transitionSpeed, {className: "+=active"}, 0)
-      .to(".cards", transitionSpeed, {height: "140px", "backgroundColor": "#f7f7f7"}, 0)
-      .to(".card", transitionSpeed, {top: 10}, 0)
-      .to(".card:not(:first-child)", 0.1, {"boxShadow": "none"}, 0)
-      .to(".transactions", transitionSpeed, {top: "140px", opacity: 1}, 0);
-
-$card.on("click", function(){
-  if(timeline.reversed()){
-    $(this).css("z-index", "2");
-    timeline.play();
-  } else {
-    timeline.reverse();
-    timeline.eventCallback("onReverseComplete", () => {
-      $(this).css("z-index", "0");
-    }); 
-  } 
-});
-
-const textElement = document.getElementById("text");
-const copyButton = document.getElementById("copy");
-
-const copyText = (e) => {
-  window.getSelection().selectAllChildren(textElement);
-  document.execCommand("copy");
-  e.target.setAttribute("tooltip", "Copied! ✅");
-};
-
-const resetTooltip = (e) => {
-  e.target.setAttribute("tooltip", "Copy to clipboard");
-};
-
-copyButton.addEventListener("click", (e) => copyText(e));
-copyButton.addEventListener("mouseover", (e) => resetTooltip(e));
-
-
-
-
-
-
 const popup = document.querySelector('.popup');
 const skipBtn = document.querySelector('.popup__skip-btn');
 const visitBtn = document.querySelector('.popup__link');
@@ -98,3 +50,32 @@ const startTimer = () => {
 if (!document.cookie.match(/^(.*;)?\s*popupCookie\s*=\s*[^;]+(.*)?$/)) {
   window.addEventListener('scroll', startTimer);
 }
+
+
+
+/*
+  Inspired from - https://dribbble.com/shots/6713486-2-FREE-Touch-Gesture-Animations-interactions-for-after-effects
+*/
+const timeline = new TimelineLite({ paused: true, reversed: true }),
+      $title = $(".cards .title"),
+      $card = $(".card-wrapper .card"),
+      $transactions = $(".transactions"),
+      transitionSpeed = .5;
+
+timeline.to(".card-wrapper", transitionSpeed, {className: "+=active"}, 0)
+      .to(".cards", transitionSpeed, {height: "140px", "backgroundColor": "#f7f7f7"}, 0)
+      .to(".card", transitionSpeed, {top: 10}, 0)
+      .to(".card:not(:first-child)", 0.1, {"boxShadow": "none"}, 0)
+      .to(".transactions", transitionSpeed, {top: "140px", opacity: 1}, 0);
+
+$card.on("click", function(){
+  if(timeline.reversed()){
+    $(this).css("z-index", "2");
+    timeline.play();
+  } else {
+    timeline.reverse();
+    timeline.eventCallback("onReverseComplete", () => {
+      $(this).css("z-index", "0");
+    }); 
+  } 
+});
